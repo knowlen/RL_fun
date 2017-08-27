b51f50dc6db760f4a8422b3be01a642457f8aec7
knowlen <knowlen@students.wwu.edu>
2017-08-26 20:51:29 -0700

    vanilla Q learning with experience replay

--- batched_rl.py
+++ batched_rl.py
@@ -8,83 +8,88 @@ import random
 import gym
 from collections import deque
 env = gym.make('CartPole-v0')
+
+""" HYPERPARAMETERS """
 learn_rate = 0.1
 mb_size = 64
 exploration_rate = 0.99 # decrease over timestep
 discount_factor = 0.001 # increase over timestep
-# hash string(state variables list) => action space
-Q_table = {}
 
+
+Q_table = {}
 replay_buffer = deque(maxlen=1000)
-#replay_buffer = []
 num_actions = env.action_space.n
 
-def add_state(S):
-    global Q_table
-    Q_table[S] = [-1.0 for i in range(num_actions)]
 
-def action(S, env):
-    global Q_table, exploration_rate
-    if S not in Q_table.keys():
-	add_state(S) 
-    
+def add_state(S):
+"""
+::  -Adds a state to the table and initializes its Q values to 0
+"""
+    Q_table[S] = [0.0 for i in range(num_actions)]
+
+
+def action(S):
+    """
+    ::  -A function to obtain the next action to take.  
+    ::  -takes: current state
+    ::  -returns: Either optimal action for state, or random
+    ::            action with chance exploration_rate.
+    """
     action_list = Q_table[S]
     if np.random.uniform(0, 1.0) >= exploration_rate:
 	v = max(action_list)
 	act = action_list.index(v)
     else:
-        act = env.action_space.sample()
-    return act 
+        act = random.choice([0, 1])
+    return int(act) 
 
-#def Q(S, a, r, S_, done):
-    #Q_table[S][a] = ((1-learn_rate) * Q_table[S][a]) + (learn_rate) * (r + discount_factor * max(Q_table[S_].values())))
 
 def Q(batch):
-   # TODO:
-   #	-Batching
-    global Q_table
+    """
+    ::  -updates: the Q values associated w/ each state in batch.
+    ::  -takes: minibatch of experience tuples (MB x 5)
+    """
     S, a, r, S_, T = batch[:,0], batch[:,1], batch[:,2], batch[:,3], batch[:,4]
+    r = r.astype(float, copy=False)
     Qs = np.asarray([float(Q_table[state][int(action)]) for state,action in zip(S, a)])
     Q_s = np.asarray([float(max(Q_table[state])) for state in S_])
-    updated_Q = ((1-learn_rate) * Qs) + (learn_rate * (r + (discount_factor * Q_s)))
+    
+    updated_Q = ((1.0-learn_rate) * Qs) + learn_rate * ((discount_factor * Q_s) + r) 
     for i, action in enumerate(a):
         state, terminal, new_value, reward = S[i], T[i], updated_Q[i], r[i]
-        if terminal:
-            Q_table[state][action] = reward
+        if terminal == True:
+            Q_table[state][int(action)] = reward
         else:
-            Q_table[state][action] = new_value
+            Q_table[state][int(action)] = new_value
 
-    #Q_table[S][a] = ((1-learn_rate) * Q_table[S][a]) + (learn_rate) * (r + discount_factor * max(Q_table[S_].values())))
 
+""" TRAINING LOOP """
 for i in range(200):
     S = str(env.reset().round(2))
-    for t in range(1000):
-        #env.render()
-         
-	if S not in Q_table.keys():
-	    add_state(S)
+    add_state(S)
 	
-	a = action(S, env) 
+    for t in range(1000):
+            
+    	a = action(S) 
 	S_, r, done, info = env.step(a)
-	S_ = str(S_.round(2))
+        env.render()
+
+        # fix the types, 
+        S_ = str(S_)
         r = float(r)
-	replay_buffer.append([S, a, r, S_, done])
+
+        # add the experience tuple to memory, 
+	replay_buffer.append([S, a, float(r), S_, done])
+
+        # put next state into the table,
 	if S_ not in Q_table.keys():
 	    add_state(S_)
-	#Q(S, a, r, S_)
-        if mb_size <= len(replay_buffer):
+
+        # once there are enough things to batch over,
+        if 3*mb_size <= len(replay_buffer):
 	    batch = np.asarray(random.sample(replay_buffer, mb_size))
             Q(batch)
-       #else:
-       #    batch = np.asarray(replay_buffer[-1]).reshape(1,5)
-       #Q(batch) 
-########for i in range(len(replay_buffer)/2):
-########    replay = replay_buffer[np.random.randint(0,len(replay_buffer))]
-########    rQ  = replay[0] 
-########    ra  = replay[1]
-########    rr  = replay[2]
-########    rS_ = replay[3]
-########    Q(rQ, ra, rr, rS_)
+
 	S = S_
         if done:
 	    print('Ep' + str(i) + ': ' + str(t) + ' steps.')
